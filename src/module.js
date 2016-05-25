b06726d9ed062078829bab7cc8029d3ba3b34cef
Simon Korn <simon.korn@textalk.se>
2016-05-25 15:40:43 +0200

    make format: base64 default to type base64file

--- src/module.js
+++ src/module.js
@@ -1,13 +1,30 @@
 angular.module('angularSchemaFormBase64FileUpload', [
   'schemaForm',
   'templates'
-]).config(['schemaFormDecoratorsProvider', 'sfBuilderProvider', function(schemaFormDecoratorsProvider, sfBuilderProvider) {
+]).config([
+  'schemaFormProvider',
+  'schemaFormDecoratorsProvider',
+  'sfBuilderProvider',
+  'sfPathProvider',
+  function(schemaFormProvider, schemaFormDecoratorsProvider, sfBuilderProvider, sfPathProvider) {
 
-  schemaFormDecoratorsProvider.defineAddOn(
-    'bootstrapDecorator',           // Name of the decorator you want to add to.
-    'file',                      // Form type that should render this add-on
-    'src/templates/angular-schema-form-base64-file-upload.html',  // Template name in $templateCache
-    sfBuilderProvider.stdBuilders   // List of builder functions to apply.
-  );
+    var base64file = function(name, schema, options) {
+      if (schema.type === 'string' && schema.format === 'base64') {
+        var f = schemaFormProvider.stdFormObj(name, schema, options);
+        f.key  = options.path;
+        f.type = 'base64file';
+        options.lookup[sfPathProvider.stringify(options.path)] = f;
+        return f;
+      }
+    };
 
-}]);
+    schemaFormProvider.defaults.string.unshift(base64file);
+
+    schemaFormDecoratorsProvider.defineAddOn(
+      'bootstrapDecorator',           // Name of the decorator you want to add to.
+      'base64file',                      // Form type that should render this add-on
+      'src/templates/angular-schema-form-base64-file-upload.html',  // Template name in $templateCache
+      sfBuilderProvider.stdBuilders   // List of builder functions to apply.
+    );
+  }
+]);
